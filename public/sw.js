c2ba1335cfe831db2db8b40fced1683cb994a97c
anhdo95 <dinhanhdo95@gmail.com>
2020-03-28 20:15:42 +0700

    Do dynamic caches in the SW

--- public/sw.js
+++ public/sw.js
@@ -1,21 +1,16 @@
-const CACHE_NAME = 'static'
+const PRE_CACHE_NAME = 'static'
+const DYNAMIC_CACHE_NAME = 'dynamic'
 
 self.addEventListener('install', function(event) {
   console.log('[Service Worker] Installing...', event)
 
   event.waitUntil(
-    caches.open(CACHE_NAME)
+    caches.open(PRE_CACHE_NAME)
       .then(function(cache) {
         cache.addAll([
           '/',
-          '/index.html',
-          '/help/',
-          '/help/index.html',
-          '/favicon.ico',
           'https://fonts.googleapis.com/css?family=Roboto:400,700',
-          'https://fonts.gstatic.com/s/roboto/v20/KFOmCnqEu92Fr1Mu4mxK.woff2',
           'https://fonts.googleapis.com/icon?family=Material+Icons',
-          'https://fonts.gstatic.com/s/materialicons/v50/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2',
           'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
           '/src/images/main-image.jpg',
           '/src/css/app.css',
@@ -39,6 +34,14 @@ self.addEventListener('fetch', function(event) {
         if (res) return res
         
         return fetch(event.request)
+          .then(function(res) {
+            return caches.open(DYNAMIC_CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request.url, res.clone())
+
+                return res
+              })
+          })
       })
   )
 })

fc2026762f1b985af8e791712fce9ca2bd33306c
anhdo <anh.dinh@savvycomsoftware.com>
2020-03-30 21:38:31 +0700

    Request notification permission

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,4 +1,5 @@
 let deferredPrompt;
+const notifications = document.querySelectorAll('.enable-notifications')
 
 window.addEventListener("DOMContentLoaded", function() {
   if (navigator.serviceWorker) {
@@ -14,4 +15,22 @@ window.addEventListener("DOMContentLoaded", function() {
     // Stash the event so it can be triggered later.
     deferredPrompt = e;
   });
+
+  if (Notification) {
+    function askForNotificationPermission() {
+      Notification.requestPermission(function(result) {
+        if (result !== 'granted') {
+          console.log('No notification permission granted.')
+        } else {
+          
+        }
+      })
+    }
+  
+    notifications.forEach(function (notification) {
+      notification.style.display = 'inline-block'
+      notification.addEventListener('click', askForNotificationPermission)
+    })
+  }
+
 });

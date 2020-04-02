bdf8bbed04e36d7fbfbce57b60aab9b98019e5cd
anhdo <anh.dinh@savvycomsoftware.com>
2020-04-02 14:26:15 +0700

    Handling background synchronisation and push notifications

--- public/sw-base.js
+++ public/sw-base.js
@@ -62,3 +62,102 @@ workboxSW.router.registerRoute(function(route) {
 
 
 workboxSW.precache([]);
+
+
+
+// Synchronization
+self.addEventListener('sync', function(event) {
+  console.log('[Service Worker] Syncing...', event)
+  
+  if (event.tag === 'sync-new-posts') {
+    database.getSyncPosts()
+      .then(function(posts) {
+        posts.forEach(function(post) {
+          const formData = new FormData()
+          formData.append('id', post.id)
+          formData.append('title', post.title)
+          formData.append('location', post.location)
+          formData.append('file', post.picture, `${post.id}.png`)
+
+          return fetch('https://us-central1-pwaprogram-3c120.cloudfunctions.net/storePostData', {
+            method: 'POST',
+            body: formData
+          })
+          .then(function(res) {
+            if (res.ok) {
+              res.json().then(function(data) {
+                return database.deleteSyncPost(data.id)
+              })
+            }
+          })
+        })
+      })
+      .catch(function(error) {
+        console.error('Error while sending data: ', error)
+      })
+  }
+})
+
+self.addEventListener('notificationclick', function(event) {
+  const notification = event.notification
+  const action = event.action
+
+  console.log('notification', notification)
+
+  if (action === 'confirm') {
+    console.log('Confirm was chosen')
+  } else {  
+    console.log('action', action)
+    event.waitUntil(
+      clients.matchAll()
+        .then(function(clis) {
+          const client = clis.find(function(c) {
+            return c.visibilityState === 'visible'
+          })
+
+          const url = (notification.data && notification.data.url) || '/'
+
+          if (client) {
+            client.navigate(url)
+            client.focus()
+          } else {
+            clients.openWindow(url)
+          }
+          
+          notification.close()
+        })
+    )
+  }
+})
+
+// This might help to collect user-analytics on the notifications
+self.addEventListener('notificationclose', function(event) {
+  console.log('Notification was closed', event)
+})
+
+self.addEventListener('push', function(event) {
+  console.log('Push Notification received', event)
+
+  let data = {
+    title: 'New!',
+    content: 'Something new happened.',
+    openUrl: '/'
+  }
+
+  if (event.data) {
+    data = JSON.parse(event.data.text())
+  }
+
+  const options = {
+    body: data.content,
+    icon: '/src/images/icons/app-icon-96x96.png',
+    badge: '/src/images/icons/app-icon-96x96.png',
+    data: {
+      url: data.openUrl
+    }
+  }
+
+  event.waitUntil(
+    self.registration.showNotification(data.title, options)
+  )
+})
